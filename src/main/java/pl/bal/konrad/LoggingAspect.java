7a24cab705a16b853777c505bd4ad89188be1712
Konrad Bal <60kondzio@gmail.com>
2016-01-09 16:24:58 +0100

    Spring AOP - after throwing advice - example

---
+++ src/main/java/pl/bal/konrad/LoggingAspect.java
@@ -0,0 +1,21 @@
+package pl.bal.konrad;
+
+import org.apache.log4j.Logger;
+
+public class LoggingAspect {
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+	
+	public void logExecution(){
+		logger.info("Method logExecution() ");
+	}
+	
+	public void warnIfNull(Object returnedValue){
+		if(returnedValue == null)
+			logger.warn("Zwracana wartosc jest nullem");
+	}
+	
+	public void serveException(RuntimeException ex){
+		logger.error("Exception: " +ex.getMessage());
+	}
+
+}
---
+++ src/main/java/pl/bal/konrad/Main.java
@@ -0,0 +1,20 @@
+package pl.bal.konrad;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+		CustomerRepository customerRepository = context.getBean("customerRepository", CustomerRepository.class);
+		
+		Customer c1 = new Customer("Janek");
+		Customer c2  = new Customer(null);
+		
+		customerRepository.addCustomer(c1);
+		customerRepository.addCustomer(c2);
+		
+	}
+
+}
